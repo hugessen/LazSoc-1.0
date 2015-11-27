dd981e6cdf1f00d57d0f14c0ca4c01c569f116b4
Richard Hugessen <richard@hugessen.ca>
2015-11-27 14:18:35 -0500

    Setting up initial modal

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,17 +2,27 @@ angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  // Form data for the login modal
   $scope.loginData = {};
-
-  // Create the login modal that we will use later
+  
+  $ionicModal.fromTemplateUrl('templates/userinfo.html', {
+    scope:$scope,
+    animation: 'slide-in-up'   
+    
+  }) 
+  .then(function(modal) {
+    $scope.userInfoModal = modal;
+  })
+  
+  $scope.openUserInfoModal = function() {
+    $scope.userInfoModal.show();
+  }
+  $scope.closeUserInfoModal = function() {
+    $scope.userInfoModal.hide();
+  }
+  $scope.$on('$destroy', function() {
+    $scope.userInfoModal.remove();
+  });
+   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
   }).then(function(modal) {
